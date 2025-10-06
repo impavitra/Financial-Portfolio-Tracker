6ab92758b3db276899a2c7007b25cec715e8c5e5
pavitramallapur <pavitramallapur123@gmail.com>
2025-10-06 14:01:18 +0530

    Fix API URL to point to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8081/api";
+const API_BASE_URL = "https://financial-portfolio-tracker.onrender.com/api";
 
 // Create axios instance
 const api = axios.create({
