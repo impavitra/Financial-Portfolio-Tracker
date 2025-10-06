d62f81ce183cab5be21baebf77749dddec10f3e5
pavitramallapur <pavitramallapur123@gmail.com>
2025-10-06 13:59:19 +0530

    Revert "Fix frontend routing and API configuration"
    This reverts commit 981bea0c6c154af06d55a0cc8f61367e6f04aa2b.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://financial-portfolio-tracker.onrender.com/api";
+const API_BASE_URL = "http://localhost:8081/api";
 
 // Create axios instance
 const api = axios.create({
